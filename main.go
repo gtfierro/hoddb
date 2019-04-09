8760fcbee6f13e3e54e234483996598d1e115056
Gabe Fierro <gtfierro225@gmail.com>
2019-04-08 23:59:19 -0700

    main.go just serves over GRPC + HTTP now

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	//	"context"
+	//	"fmt"
 	"git.sr.ht/~gabe/hod/hod"
-	"github.com/chzyer/readline"
+	//	"github.com/chzyer/readline"
 	"github.com/pkg/errors"
-	"io"
+	//	"io"
 	"log"
-	"strings"
-	"time"
+	//	"strings"
+	//	"time"
 )
 
 var debug = false
@@ -58,7 +58,7 @@ func main() {
 
 	//q := "SELECT ?vav ?room FROM test WHERE { ?vav rdf:type brick:VAV . ?room rdf:type brick:Room . ?zone rdf:type brick:HVAC_Zone . ?vav bf:feeds+ ?zone . ?room bf:isPartOf ?zone };"
 
-	q := "SELECT ?x ?y FROM * WHERE { ?r rdf:type brick:Room . ?x ?y ?r };"
+	//q := "SELECT ?x ?y FROM * WHERE { ?r rdf:type brick:Room . ?x ?y ?r };"
 	//version, err := L.LoadFile("soda", "BrickFrame.ttl", "brickframe")
 	//if err != nil {
 	//	log.Fatal(errors.Wrap(err, "load brickframe"))
@@ -93,74 +93,89 @@ func main() {
 	//}
 	//L.Dump(entity)
 
-	prompt, err := readline.NewEx(&readline.Config{
-		Prompt:            "\033[32m»\033[0m ",
-		HistoryFile:       "/tmp/readline.tmp",
-		EOFPrompt:         "exit",
-		HistorySearchFold: true,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer prompt.Close()
-	//	prompt.HistoryDisable()
+	//go func() {
+	log.Fatal(L.ServeGRPC())
+	//}()
 
-	var buildup = ""
-	ts := time.Now().UnixNano()
-	for {
-		line, err := prompt.Readline()
-		if err == readline.ErrInterrupt {
-			if len(line) == 0 {
-				break
-			} else {
-				continue
-			}
-		} else if err == io.EOF {
-			break
-		}
-
-		buildup += strings.TrimSpace(line)
-		if strings.HasSuffix(buildup, ";") {
-			prompt.SetPrompt("\033[32m»\033[0m ")
-			query := buildup
-			buildup = ""
-			if err = prompt.SaveHistory(query); err != nil {
-				log.Fatal(err)
-			}
-
-			selectquery, err := L.ParseQuery(query, ts)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			res, err := L.Select(context.Background(), selectquery)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			fmt.Fprintf(prompt, "rows %d\n", len(res.Rows))
-
-			prompt.SetPrompt("\033[32m»\033[0m ")
-		} else if len(buildup) > 0 {
-			prompt.SetPrompt("...  ")
-		}
-	}
-
-	selectquery, err := L.ParseQuery(q, ts)
-	log.Println(selectquery)
-
-	_, err = L.Select(context.Background(), selectquery)
-	if err != nil {
-		log.Println(errors.Wrap(err, "select q1"))
-	} else {
-		log.Println("successful q1")
-	}
-	//cur.dumpResponse(resp)
-
-	_, err = L.Select(context.Background(), selectquery)
-	if err != nil {
-		log.Println(errors.Wrap(err, "select q2"))
-	} else {
-		log.Println("successful q2")
-	}
+	//	if x, e := L.ParseQuery("SELECT ?x FROM movie WHERE { ?x rdf:type ?y };", time.Now().UnixNano()); e != nil {
+	//		log.Fatal(e)
+	//	} else {
+	//		log.Println(x)
+	//	}
+	//	//if res, err := L.Select(context.Background(), x); err != nil {
+	//	//	log.Fatal(e)
+	//	//} else {
+	//	//	log.Println(x)
+	//	//}
+	//
+	//	prompt, err := readline.NewEx(&readline.Config{
+	//		Prompt:            "\033[32m»\033[0m ",
+	//		HistoryFile:       "/tmp/readline.tmp",
+	//		EOFPrompt:         "exit",
+	//		HistorySearchFold: true,
+	//	})
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	defer prompt.Close()
+	//	//	prompt.HistoryDisable()
+	//
+	//	var buildup = ""
+	//	ts := time.Now().UnixNano()
+	//	for {
+	//		line, err := prompt.Readline()
+	//		if err == readline.ErrInterrupt {
+	//			if len(line) == 0 {
+	//				break
+	//			} else {
+	//				continue
+	//			}
+	//		} else if err == io.EOF {
+	//			break
+	//		}
+	//
+	//		buildup += strings.TrimSpace(line)
+	//		if strings.HasSuffix(buildup, ";") {
+	//			prompt.SetPrompt("\033[32m»\033[0m ")
+	//			query := buildup
+	//			buildup = ""
+	//			if err = prompt.SaveHistory(query); err != nil {
+	//				log.Fatal(err)
+	//			}
+	//
+	//			selectquery, err := L.ParseQuery(query, ts)
+	//			if err != nil {
+	//				log.Println(err)
+	//				continue
+	//			}
+	//			res, err := L.Select(context.Background(), selectquery)
+	//			if err != nil {
+	//				log.Println(err)
+	//				continue
+	//			}
+	//			fmt.Fprintf(prompt, "rows %d\n", len(res.Rows))
+	//
+	//			prompt.SetPrompt("\033[32m»\033[0m ")
+	//		} else if len(buildup) > 0 {
+	//			prompt.SetPrompt("...  ")
+	//		}
+	//	}
+	//
+	//	selectquery, err := L.ParseQuery(q, ts)
+	//	log.Println(selectquery)
+	//
+	//	_, err = L.Select(context.Background(), selectquery)
+	//	if err != nil {
+	//		log.Println(errors.Wrap(err, "select q1"))
+	//	} else {
+	//		log.Println("successful q1")
+	//	}
+	//	//cur.dumpResponse(resp)
+	//
+	//	_, err = L.Select(context.Background(), selectquery)
+	//	if err != nil {
+	//		log.Println(errors.Wrap(err, "select q2"))
+	//	} else {
+	//		log.Println("successful q2")
+	//	}
 }
