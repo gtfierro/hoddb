09c8ef0fea50d6c67b6ac0bcb134646030ee3715
Gabe Fierro <gtfierro225@gmail.com>
2019-10-09 20:48:29 -0700

    network driver initial

---
+++ drivers/network/main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+
+	"github.com/gtfierro/hoddb/p2p"
+	rdf "github.com/gtfierro/hoddb/turtle"
+
+	"github.com/dchest/uniuri"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/examples/util"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"github.com/pkg/errors"
+)
+
+const (
+	OWL     Namespace = "http://www.w3.org/2002/07/owl#"
+	RDF     Namespace = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+	RDFS    Namespace = "http://www.w3.org/2000/01/rdf-schema#"
+	NETWORK Namespace = "http://xbos.io/ontologies/network#"
+	MYNET   Namespace = "http://example.com/mynet#"
+)
+
+var cache = make(map[rdf.URI]rdf.URI)
+
+//var newId(value string) rdf.URI {
+//    if uri, found := cache[
+//}
+
+type Namespace string
+
+func (ns Namespace) URI(resource string) rdf.URI {
+	return rdf.ParseURI(string(ns) + resource)
+}
+func Value(resource string) rdf.URI {
+	return rdf.URI{Value: fmt.Sprintf("\"%s\"", resource)}
+}
+
+func (mon *NetworkMonitor) TriplesFromPacket(pkt gopacket.Packet) (triples []rdf.Triple) {
+	// get addresses from network layer
+
+	// generate random identifiers; we can resolve these later
+	var src = MYNET.URI(uniuri.New())
+	var dst = MYNET.URI(uniuri.New())
+
+	addTriple := func(defaultsub, pred, obj rdf.URI) {
+		ents := mon.getEntitiesWithProperty(pred, obj)
+		if len(ents) > 0 {
+			defaultsub = ents[0]
+		}
+		triples = append(triples, rdf.Triple{
+			Subject:   defaultsub,
+			Predicate: pred,
+			Object:    obj,
+		})
+	}
+
+	// decode link layer
+	var linkflow gopacket.Flow
+	if m, ok := pkt.LinkLayer().(*layers.Ethernet); ok {
+		linkflow = m.LinkFlow()
+	} else {
+		panic("no network layer")
+	}
+
+	addTriple(src, NETWORK.URI("hasMAC"), Value(linkflow.Src().String()))
+	addTriple(dst, NETWORK.URI("hasMAC"), Value(linkflow.Dst().String()))
+
+	// test
+
+	// decode network layer
+	var networkflow gopacket.Flow
+	if net6, ok := pkt.NetworkLayer().(*layers.IPv6); ok {
+		networkflow = net6.NetworkFlow()
+	} else if net4, ok := pkt.NetworkLayer().(*layers.IPv4); ok {
+		networkflow = net4.NetworkFlow()
+	} else {
+		panic("no network layer")
+	}
+	addTriple(src, NETWORK.URI("hasAddress"), Value(networkflow.Src().String()))
+	addTriple(dst, NETWORK.URI("hasAddress"), Value(networkflow.Dst().String()))
+
+	// decode transport layer
+	var transportflow gopacket.Flow
+	var relship rdf.URI
+	if tcp, ok := pkt.TransportLayer().(*layers.TCP); ok {
+		transportflow = tcp.TransportFlow()
+		relship = NETWORK.URI("hasTCPPort")
+	} else if udp, ok := pkt.TransportLayer().(*layers.UDP); ok {
+		transportflow = udp.TransportFlow()
+		relship = NETWORK.URI("hasUDPPort")
+	} else {
+		panic("no  transport layer")
+	}
+	addTriple(src, relship, Value(transportflow.Src().String()))
+	addTriple(dst, relship, Value(transportflow.Dst().String()))
+
+	return
+}
+
+// detect hosts on the network ;get the manufacturer using the OID
+// detect UPNP services
+
+// generate triples from what we notice
+func doScrape(c *cli.Context) error {
+	fmt.Println(c.FlagNames())
+
+	// get config
+	f, err := os.Open(c.String("config"))
+	if err != nil {
+		return errors.Wrap(err, "read config file")
+	}
+	cfg, err := p2p.ReadConfig(f)
+	if err != nil {
+		return errors.Wrap(err, "parse config file")
+	}
+
+	mon, err := NewNetworkMonitor(&cfg)
+	if err != nil {
+		return errors.Wrap(err, "create network monitor")
+	}
+
+	// dump database
+	var __select_all_query = `SELECT ?s ?p ?o WHERE { ?s ?p ?o };`
+	resp, err := mon.runQuery(__select_all_query)
+	if err != nil {
+		panic(err)
+	}
+	if err != nil {
+		panic(err)
+	}
+	for _, t := range resp {
+		fmt.Println(t)
+	}
+	fmt.Println("-------------")
+
+	defer util.Run()()
+	if handle, err := pcap.OpenLive(c.String("interface"), 1600, true, pcap.BlockForever); err != nil {
+		return errors.Wrap(err, "could not open iface")
+	} else if err := handle.SetBPFFilter(c.String("filter")); err != nil { // optional
+		return errors.Wrap(err, "could not set bpf filter")
+	} else {
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+		for packet := range packetSource.Packets() {
+			if packet.NetworkLayer() == nil || packet.TransportLayer() == nil {
+				continue
+			}
+			pkttriples := mon.TriplesFromPacket(packet)
+			for _, t := range pkttriples {
+				fmt.Printf("Triple>> %s\n", t)
+			}
+			if err := mon.AddTriples(pkttriples); err != nil {
+				panic(err)
+			}
+
+		}
+	}
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "network"
+	app.Commands = []cli.Command{
+		{
+			Name:  "listen",
+			Usage: "Scrape traffic off of an interface ",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "interface, i",
+					Usage: "Interface to scrape packets from",
+					Value: "eth0",
+				},
+				cli.StringFlag{
+					Name:  "filter, f",
+					Usage: "BPF filter",
+					Value: "ip",
+				},
+				cli.StringFlag{
+					Name:  "config, c",
+					Usage: "Config file",
+					Value: "networknode.toml",
+				},
+			},
+			Action: doScrape,
+		},
+	}
+	log.Println(os.Args)
+	log.Fatal(app.Run(os.Args))
+}
---
+++ drivers/network/monitor.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gtfierro/hoddb/hod"
+	"github.com/gtfierro/hoddb/p2p"
+	rdf "github.com/gtfierro/hoddb/turtle"
+	"github.com/pkg/errors"
+)
+
+type NetworkMonitor struct {
+	node *p2p.Node
+	db   *hod.HodDB
+	cfg  *p2p.Config
+}
+
+func NewNetworkMonitor(cfg *p2p.Config) (*NetworkMonitor, error) {
+	var err error
+	mon := new(NetworkMonitor)
+	mon.cfg = cfg
+	mon.node, err = p2p.NewNode(cfg)
+	//TODO: method
+	if err != nil {
+		return nil, errors.Wrap(err, "make hoddb")
+	}
+	mon.db = mon.node.GetHodDB()
+	return mon, nil
+}
+
+func (mon *NetworkMonitor) getEntitiesWithProperty(pred, obj rdf.URI) []rdf.URI {
+	q := fmt.Sprintf(`SELECT ?e WHERE { ?e <%s> <%s> };`, pred, obj)
+	res, err := mon.runQuery(q)
+	if err != nil {
+		panic(err)
+	}
+	var ret []rdf.URI
+	for _, r := range res {
+		ret = append(ret, r[0])
+	}
+	return ret
+}
+
+func (mon *NetworkMonitor) runQuery(qstr string) ([][]rdf.URI, error) {
+	var res [][]rdf.URI
+	q, err := mon.db.ParseQuery(qstr, 0)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := mon.db.Select(context.Background(), q)
+	if err != nil {
+		return nil, err
+	}
+	for _, row := range resp.Rows {
+		var nr []rdf.URI
+		for _, u := range row.Values {
+			nr = append(nr, rdf.URI{Namespace: u.Namespace, Value: u.Value})
+		}
+		res = append(res, nr)
+	}
+	return res, nil
+}
+
+func (mon *NetworkMonitor) AddTriples(triples []rdf.Triple) error {
+	var ds = rdf.NewDataSet()
+	ds.Triples = triples
+	_, err := mon.db.AddTriples("test", *ds)
+	fmt.Println("added triples", len(triples))
+	return err
+}
